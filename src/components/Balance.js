2dbd718e7aca7c7e44ae2cb119b2e6c2a607ed12
mark727221029@gmail.com <mark727221029@gmail.com>
2023-10-16 21:32:21 +1100

    first commit

---
+++ src/components/Balance.js
@@ -0,0 +1,25 @@
+import { useContext, useEffect,useState } from "react";
+import {GlobalContext} from '../context/GlobalState';
+export const Balance = ()=>{
+  const {transactions} = useContext(GlobalContext);
+  const [balance, setBalance] = useState(0)
+
+  useEffect(()=>{
+     const totalAmounts = transactions
+     .map(transaction=>transaction.amount)
+     .reduce((acc,item)=>(acc += item), 0)
+     .toFixed(2)
+
+     console.log('totalAmounts', totalAmounts)
+     setBalance(totalAmounts)
+  },[transactions])
+
+
+  
+  return(
+    <>
+      <h4>Your Balance</h4>
+      <h1>${balance}</h1>
+    </>
+  )
+}
---
+++ src/context/GlobalState.js
@@ -0,0 +1,50 @@
+import React, {createContext, useReducer} from 'react';
+import AppReducer from './AppReducer';
+
+
+
+const initialState = {
+  transactions:[
+    {id:1, text:'Flower', amount:-20},
+    {id:2, text:'Salary', amount:300},
+    {id:3, text:'Book', amount:-10}
+  ]
+}
+
+
+//create context
+export const GlobalContext = createContext(initialState)
+
+
+//provider component
+export const GlobalProvider = ({children})=>{
+  const [state,dispatch] = useReducer(AppReducer, initialState)
+
+  //actions
+  function deleteTransaction(id){
+    dispatch({
+      type:'DELETE_TRANCTION',
+      payload:id
+    })
+  }
+
+  function addTransaction(transaction){
+    dispatch({
+      type:'ADD_TRANCTION',
+      payload:transaction
+    })
+  }
+
+
+  return(
+     <GlobalContext.Provider
+      value={{
+        transactions:state.transactions,
+        deleteTransaction,
+        addTransaction
+      }}
+     >
+        {children}
+     </GlobalContext.Provider>
+  )
+}
